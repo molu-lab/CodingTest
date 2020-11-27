d6ddc65659c835df742127cd457822219646f48b
Seung.workspace@gmail.com <Seung.workspace@gmail.com>
2020-11-27 22:01:38 +0900

    test

--- java/algorithm/filename_search.java
+++ java/algorithm/filename_search.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class filename_search {
+import java.util.ArrayList;
+import java.util.List;
+
+public class filename_search {
+    public static void main(String[] args)
+    {
+    }
+
+
+    public static List<String> substring_names(String[] files)
+    {
+        List<String> list=new ArrayList<String>();
+        for(String str: files)
+        {
+            str=str.replaceAll(" ","");
+            str=str.replaceAll("-","");
+
+            str=str.split("\\.")[0];
+            System.out.println(str);
+            list.add(str);
+        }
+
+        return list;
+    }
+    public static int[] getNumber(String str,int s)
+    {
+        int[] pointer=new int[2];
+        pointer[0]=s;
+        for(int i=s;i<str.length();i++)
+        {
+            if(!Character.isDigit(str.charAt(i)))
+            {
+                pointer[1]=i;
+                break;
+            }
+        }
+        return pointer;
+    }
 }
